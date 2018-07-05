b15bd84f05fbd55bba83ae59900e0bcc18da9df2
David Hain <dhain@zognot.org>
2018-07-05 14:11:59 -0400

    Dump in yaml format

--- enkube/dump.py
+++ enkube/dump.py
@@ -1,7 +1,7 @@
 import click
 
 from .enkube import pass_env
-from .util import format_json
+from .util import format_yaml
 from .api import Api
 
 
@@ -11,6 +11,4 @@ def cli(env):
     '''Dump all objects from Kubernetes server.'''
     with Api(env) as api:
         for obj in api.walk():
-            formatted = format_json(obj)
-            click.secho('---', fg='blue')
-            click.echo(formatted)
+            click.echo(format_yaml(obj))
